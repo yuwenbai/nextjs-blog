1dac25a947bc48392454e93fa6601d6d8b711c6e
jinyuzhao <zhaojinyu@huohua.cn>
2021-09-02 11:29:53 +0800

    remove tailwindcss

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c691a642f1924217968"; }
+/******/ 		__webpack_require__.h = function() { return "ac419e29e11fe9d400ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.bc6c47fd5eead8960b1b.hot-update.js
+++ .next/static/webpack/webpack.bc6c47fd5eead8960b1b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "85ad1e68540741e8d87a"; }
+/******/ 	__webpack_require__.h = function() { return "38b1db28abf04c04b9e6"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
